36c756de495c1c0a58d286de1a6d5b15d78c872e
kimsohee1221 <taesoofly@naver.com>
2023-05-12 00:20:28 +0900

    5월 11일 김소희

--- app/src/main/java/com/example/moapp_project/MainActivity.java
+++ app/src/main/java/com/example/moapp_project/MainActivity.java
@@ -1,19 +1,17 @@
 package com.example.moapp_project;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -32,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         if (user == null) {
             myStartActivity(SignUpActivity.class);
         } else {
+            myStartActivity(CameraActivity.class);
             FirebaseFirestore db = FirebaseFirestore.getInstance();
             DocumentReference docRef = db.collection("users").document(user.getUid());
             docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
